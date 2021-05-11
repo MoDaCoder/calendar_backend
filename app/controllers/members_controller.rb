466f7d46bacddbed4961451faea27f3ee47fa291
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-11 15:54:24 -0500

    Adding members index method in members controller.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,8 +1,12 @@
 class MembersController < ApplicationController
-  before_action :authenticate_member!
+  # before_action :authenticate_member!
+
+  def index
+    @members = Member.all 
+  end
 
   private 
   def member_params
-    params.require(:member).permit(:first_name, :last_name, pending_approvals_attributes:[:id, :status], reservations_attributes:[:id, :start_date, :end_date, :total_day])
+    params.require(:member).permit(:id, :first_name, :last_name, pending_approvals_attributes:[:id, :status], reservations_attributes:[:id, :start_date, :end_date, :total_day])
   end
 end
