74acf2b1a8c278fed6a611b0d0ddac58b7429e4e
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-11 14:59:14 -0500

    Filling in member_params with attributes.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,5 +3,6 @@ class MembersController < ApplicationController
 
   private 
   def member_params
+    params.require(:member).permit(:first_name, :last_name, pending_approvals_attributes:[:id, :status], reservations_attributes:[:id, :start_date, :end_date, :total_day])
   end
 end
