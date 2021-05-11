237afae139f3a93aa673fb86ffb5700354143eb4
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-11 13:56:59 -0500

    Changing reservations table attribute total_days to total_day to make it singular.

--- db/migrate/20210511152629_create_resrvations.rb
+++ db/migrate/20210511152629_create_resrvations.rb
@@ -3,7 +3,7 @@ class CreateResrvations < ActiveRecord::Migration[6.1]
     create_table :reservations do |t|
       t.date :start_date
       t.date :end_date
-      t.integer :total_days
+      t.integer :total_day
       t.references :housing, null: false, foreign_key: true
       t.references :member, null: false, foreign_key: true
 
