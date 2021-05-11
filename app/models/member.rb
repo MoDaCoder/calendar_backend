9c337bcf08c191137f437d6b038f27abc0818769
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-11 13:46:20 -0500

    Adding nested attribute functionality to models.

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,10 @@ class Member < ApplicationRecord
   has_many :pending_approvals
   # has_many :reservations, through: :pending_approvals
   has_many :housings, through: :reservations
+
+  # May also want to set error handling incase nested attributes are blank
+  accepts_nested_attributes_for :reservations, :pending_approvals
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,5 @@ class Reservation < ApplicationRecord
   belongs_to :housing
   belongs_to :member
   has_many :pending_approvals
+  accepts_nested_attributes_for :pending_approvals
 end
