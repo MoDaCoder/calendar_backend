bd0def4e5e76f4ddd3c533522795cdc2cba66fe9
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-12 15:58:48 -0500

    Creating member_reservations and housing_reservations nested routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :members, controllers: { sessions: 'members/sessions' }
 
-  resources :members
-  resources :housings
+  resources :members do
+    resources :reservations
+  end
+
+  resources :housings do
+    resources :reservations
+  end
 end
