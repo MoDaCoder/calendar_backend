b08d7a95276d45e51132dd562bb424a253f65d59
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-12 10:51:47 -0500

    Overriding devise's default function and passing nested attributes in the strong params.

--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -2,6 +2,8 @@
 
 class Members::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_permitted_params, if: :devise_controller?
+
 
   # GET /resource/sign_in
   def new
@@ -21,7 +23,12 @@ class Members::SessionsController < Devise::SessionsController
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
+
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
+
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, pending_approvals_attributes:[:id, :status], reservations_attributes:[:id, :start_date, :end_date, :total_day]])
   end
 end
