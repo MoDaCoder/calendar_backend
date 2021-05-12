f2e95ef546812e08fdde234625bb4eefabb58367
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-12 14:33:45 -0500

    Generating housing controller and creating housing strong params.

---
+++ app/controllers/housings_controller.rb
@@ -0,0 +1,8 @@
+class HousingsController < ApplicationController
+
+    private 
+    
+    def housing_params
+        params.require(:housing).permit(:address, :city, :zip, :img, pending_approvals_attributes:[:id, :status], reservations_attributes:[:id, :start_date, :end_date, :total_day])
+    end
+end
