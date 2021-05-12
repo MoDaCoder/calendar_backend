b0fe93c033007cd2bf5d95a2a9d40bcb0743d7ad
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-12 16:00:58 -0500

    Adding housing and member id to reservations strong params.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,6 +3,6 @@ class ReservationsController < ApplicationController
     private
 
     def reservation_params
-        params.require(:reservation).permit(:start_date, :end_date, :total_day, pending_approvals_attributes:[:id, :status])
+        params.require(:reservation).permit(:start_date, :end_date, :total_day, :housing_id, :member_id, pending_approvals_attributes:[:id, :status])
     end
 end
