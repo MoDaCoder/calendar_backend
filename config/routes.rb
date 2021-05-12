eea4654d1c602e70afc1cb57974fa7192841a211
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-12 08:41:44 -0500

    Changing members routes so it uses members sessions controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'members/member_params'
-  get 'members/index'
-  devise_for :members
+  # get 'members/member_params'
+  # get 'members/index'
+  # devise_for :members
 
-  match '/members', to: 'members#index', via: 'get'
+  # match '/members', to: 'members#index', via: 'get'
+
+  devise_for :members, controllers: { sessions: 'members/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
