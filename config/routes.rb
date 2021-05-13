4081548246158a28c865d9bc20b14fddbc1c5713
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-13 15:46:20 -0500

    Removing dead code from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :members, controllers: { sessions: 'members/sessions' }
-  # as :member do 
-  #   get 'members', :to => 'members/sessions#index'
-  # end
 
   resources :members do
     resources :reservations do
